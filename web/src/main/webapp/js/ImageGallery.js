f9165ab3dcf9ee79c02b49ee412ffb81d308443f
Dave Clay <daveclay@gmail.com>
2017-01-13 12:47:35 -0800

    Fix tag info stuff when linking

--- web/src/main/webapp/js/ImageGallery.js
+++ web/src/main/webapp/js/ImageGallery.js
@@ -1,4 +1,35 @@
-function ImageGallery(imageApi, loader, thumbnailsElem) {
+function ImageGallery(imageApi, loader, tagInfoElem, thumbnailsElem) {
+    var tagInfo = {
+        "figure constructions":
+        "The figure constructions are explorations in engineering art, building up abstract " +
+        "sculptural elements integrated into figures rendered with mixed media. <i>\"There's a lot of chaotic elements " +
+        "in these pieces. I tried to construct a certain amount of decayed industrial textures and figurative energy.\"</i> " +
+        "These figures represent a transition between the digital collage and the artist's current figurative oil paintings.",
+
+        "figure painting":
+        "The artist's current primary focus is painting the figure in oils. <i>\"I'm looking at using oils - " +
+        "its variety in color and texture - to further explore the complex relationships of humanity, sexuality, and our digital-industrial " +
+        "environment. The figures drift in and out of abstract, ambient textures rather than existing in a well defined " +
+        "space.\"</i>",
+
+        "abstracts":
+        "<i>\"Abstraction is always at the edge of even my figurative works. I realized that I wasn't interested " +
+        "in literal transcriptions or realism. I want to reflect a broader context of the environment that we live in." +
+        "\"</i> The abstract pieces here represent experiments and studies, elements that may be brought into the figurative works.",
+
+        "digital collage":
+        "These digital works, created from 2001-2007, examine the ephemeral nature of modern sexuality and technology. " +
+        "The source images are assembled into abstract bio-mechanical forms with explicit and suggestive figurative " +
+        "elements. These images combined the artist's fascination with code, engineering and figurative work.",
+
+        "posters": "These posters were created for various local bands using various pen and ink techniques and digitally colored.",
+
+        "sculpture": "<i>\"Sculpture is rarely a focus of my work, but has had a major influence on how I approach drawing" +
+        " and painting. It's a tool for me to delve further into industrial forms with new media.\"</i>",
+
+        "graphic design": "Album covers and websites for various personal and professional projects."
+    };
+
     var self = this;
     var containerElem = $('#photoswipe');
 
@@ -7,6 +38,8 @@ function ImageGallery(imageApi, loader, thumbnailsElem) {
     };
 
     this.loadImagesForTag = function (tag) {
+        thumbnailsElem.empty();
+        tagInfoElem.html(tagInfo[tag] || "");
         return imageApi.loadImagesForTagAnd(tag, function (images) {
             this.handleLoadImages(tag, images);
         }.bind(this));
--- web/src/main/webapp/js/retorx.js
+++ web/src/main/webapp/js/retorx.js
@@ -1,34 +1,3 @@
-var tagInfo = {
-    "figure constructions":
-        "The figure constructions are explorations in engineering art, building up abstract " +
-        "sculptural elements integrated into figures rendered with mixed media. <i>\"There's a lot of chaotic elements " +
-        "in these pieces. I tried to construct a certain amount of decayed industrial textures and figurative energy.\"</i> " +
-        "These figures represent a transition between the digital collage and the artist's current figurative oil paintings.",
-
-    "figure painting":
-        "The artist's current primary focus is painting the figure in oils. <i>\"I'm looking at using oils - " +
-        "its variety in color and texture - to further explore the complex relationships of humanity, sexuality, and our digital-industrial " +
-        "environment. The figures drift in and out of abstract, ambient textures rather than existing in a well defined " +
-        "space.\"</i>",
-
-    "abstracts":
-        "<i>\"Abstraction is always at the edge of even my figurative works. I realized that I wasn't interested " +
-        "in literal transcriptions or realism. I want to reflect a broader context of the environment that we live in." +
-        "\"</i> The abstract pieces here represent experiments and studies, elements that may be brought into the figurative works.",
-
-    "digital collage":
-        "These digital works, created from 2001-2007, examine the ephemeral nature of modern sexuality and technology. " +
-        "The source images are assembled into abstract bio-mechanical forms with explicit and suggestive figurative " +
-        "elements. These images combined the artist's fascination with code, engineering and figurative work.",
-
-    "posters": "These posters were created for various local bands using various pen and ink techniques and digitally colored.",
-
-    "sculpture": "<i>\"Sculpture is rarely a focus of my work, but has had a major influence on how I approach drawing" +
-        " and painting. It's a tool for me to delve further into industrial forms with new media.\"</i>",
-
-    "graphic design": "Album covers and websites for various personal and professional projects."
-};
-
 var initialTag = "figure painting";
 
 $(document).ready(function() {
@@ -50,7 +19,7 @@ $(document).ready(function() {
     sidebarTagMenu.load();
     navbarTagMenu.load();
 
-    var imageGallery = new ImageGallery(imageApi, loader, contentElem);
+    var imageGallery = new ImageGallery(imageApi, loader, tagInfoElem, contentElem);
     body.append(imageGallery.getElement());
 
     var about = new AboutInfo(baseServicePath, contentElem);
@@ -77,11 +46,9 @@ $(document).ready(function() {
     var tagSelected = function(tag) {
         navbarButtonText.html(tag);
         loader.show();
-        imageGallery.clear();
-        tagInfoElem.html(tagInfo[tag] || "");
-        contentElem.empty();
         if (tag == "about") {
             loader.hide();
+            imageGallery.clear();
             about.show();
         } else {
             imageGallery.loadImagesForTag(tag);
@@ -101,7 +68,6 @@ $(document).ready(function() {
         })
     } else {
         imageGallery.loadImagesForTag(initialTag);
-        tagInfoElem.html(tagInfo[initialTag]);
     }
 
     setTimeout(function() { window.scrollTo(0, 1); }, 2000);

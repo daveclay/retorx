b847423f7d912a908d31350ef07dc316f83aefd7
Dave Clay <daveclay@gmail.com>
2017-05-26 14:04:00 -0700

    Hide dropdown menu on anything but xs

--- web/src/main/js/main/components/App.jsx
+++ web/src/main/js/main/components/App.jsx
@@ -37,7 +37,7 @@ const App = ({
       <Col xs={4} md={6}>
         <div className="title">Dave Clay</div>
       </Col>
-      <Col xs={8} md={6} mdHidden>
+      <Col xs={8} smHidden mdHidden lgHidden>
         <DropdownMenu/>
       </Col>
     </Navbar>
